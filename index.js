70bbc5861b9aa4db9f03a19f4ce5d5d0c42bd887
James Lee <j.lee1203@hotmail.com>
2022-10-10 11:12:20 -0700

    Just added some thoughts and ideas that I do not want to forget at a later time when tackling these obstacles.

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const pokeWeight = document.createElement('id');
 
 
 // Constants, helper functions, URL, etc.
-const base_URL = https://pokeapi.co/api/v2/;
+const base_URL = "https://pokeapi.co/api/v2/";
 
+//The callback functions that will preform what I want for the event listeners.
+
+
+//Questions/Ideas/Things to Not Forget
+// - How to implement search function
+// - Will I need to use different api endpoints for different info on each pokemon?
+// - An event for toggling night mode?
+// - Can I search a pokemon, then "like or catch" instance of, to save to my pokedex?
