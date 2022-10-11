b066d87651202368162d84b59e029820edd0a57c
James Lee <j.lee1203@hotmail.com>
2022-10-11 11:21:29 -0700

    Fixed up the DOM elements being created and used DOM manipulation to fill in their attributes and content, then appended to pokeCard, then pokeDex.

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
 //DOM Elements creation
-// function renderPokemon(){
-// const pokeDex = document.createElement('div');
-// const pokeCard = document.createElement('div');
+function renderPokemon(data){
+const pokeDex = document.createElement('div');
+const pokeCard = document.createElement('div');
     
-// const pokeName = document.createElement('h1');
-// const pokeId = document.createElement('div');
-// const pokeImage = document.createElement('div');
-// const pokeTypeOne = document.createElement('div');
-// const pokeTypeTwo = document.createElement('div');
-// const pokeHeight = document.createElement('div');
-// const pokeWeight = document.createElement('div');
+const pokeName = document.createElement('h1');
+const pokeId = document.createElement('p');
+const pokeImage1 = document.createElement('img');
+const pokeImage2 = document.createElement('img');
+const pokeHeight = document.createElement('p');
+const pokeWeight = document.createElement('p');
 
 //Content/attributes for each newly created DOM element.
-
-
-
-
+pokeName.textContent = data.name;
+pokeId.textContent = data.id;
+pokeImage1.src = data.sprites.front_default;
+pokeImage2.src = data.sprites.back_default;
+pokeHeight.textContent = data.height;
+pokeWeight.textContent = data.weight;
 
 
 //Appending all the correctly nested elements within each parent node. Not seen in HTML file, the created 
 //elements, used js to apply dom manipulation for insertion of each element.
-// document.body.appendChild(pokeDex);    
-// pokeDex.append(pokeCard);
-// pokeCard.append(pokeName, pokeId, pokeImage, pokeTypeOne, pokeTypeTwo, pokeHeight, pokeWeight);
-// }
+document.body.appendChild(pokeDex);    
+pokeDex.append(pokeCard);
+pokeCard.append(pokeName, pokeId, pokeImage1, pokeImage2, pokeHeight, pokeWeight);
+}
 
-
-
-
-        const base_URL = `https://pokeapi.co/api/v2/pokemon/ditto`;
-        fetch(base_URL)
-        .then(resp => resp.json())
-        .then(data => {
-            console.log(data);
-            
-        });  
+const base_URL = `https://pokeapi.co/api/v2/pokemon/1`;
+fetch(base_URL)
+.then(resp => resp.json())
+.then(data => {
+    console.log(data)
+    renderPokemon(data)
+});  
 
 
 
