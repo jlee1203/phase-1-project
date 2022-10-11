fe94ff53d1450d4db132244897a997006e37ca9c
James Lee <j.lee1203@hotmail.com>
2022-10-10 18:42:30 -0700

    Restructured my html file and starting it over to not be as dependent on DOM manipulation. Now able to add in content to those DOM elements and easily append them to respective id's in the DOM.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// DOM Elements creation
-
-    const pokeDex = document.createElement('div');
-    const pokeCard = document.createElement('div');
+//DOM Elements creation
+// function renderPokemon(){
+// const pokeDex = document.createElement('div');
+// const pokeCard = document.createElement('div');
     
-    const pokeName = document.createElement('h1');
-    const pokeId = document.createElement('div');
-    const pokeImage = document.createElement('div');
-    const pokeType = document.createElement('div');
-    const pokeHeight = document.createElement('div');
-    const pokeWeight = document.createElement('div');
+// const pokeName = document.createElement('h1');
+// const pokeId = document.createElement('div');
+// const pokeImage = document.createElement('div');
+// const pokeTypeOne = document.createElement('div');
+// const pokeTypeTwo = document.createElement('div');
+// const pokeHeight = document.createElement('div');
+// const pokeWeight = document.createElement('div');
 
+//Content/attributes for each newly created DOM element.
 
 
 
@@ -18,21 +20,23 @@
 
 //Appending all the correctly nested elements within each parent node. Not seen in HTML file, the created 
 //elements, used js to apply dom manipulation for insertion of each element.
-document.body.appendChild(pokeDex);    
-pokeCard.append(pokeName, pokeId, pokeImage, pokeType, pokeHeight, pokeWeight);
-pokeDex.append(pokeCard);
-
-    
-    
-    
+// document.body.appendChild(pokeDex);    
+// pokeDex.append(pokeCard);
+// pokeCard.append(pokeName, pokeId, pokeImage, pokeTypeOne, pokeTypeTwo, pokeHeight, pokeWeight);
+// }
 
 
 
-// fetch("https://pokeapi.co/api/v2/pokemon/")
-// .then(resp => resp.json())
-// .then(data => {
-//     console.log(data)
-// });
+
+        const base_URL = `https://pokeapi.co/api/v2/pokemon/ditto`;
+        fetch(base_URL)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            
+        });  
+
+
 
 
 
@@ -52,3 +56,5 @@ pokeDex.append(pokeCard);
 // - Adding in battle stats? (ATT/DEF/STAMINA)
 // - Pokemon not yet added in, show up as ? After searched, liked to add to pokedex, then name/image pop up.
 // - Background color changes to match with pokemon type?
+// - How to use the pokemon api .../1 for each pokemon, to get all the id, name, moves, etc...
+// and access those in api object to plug in their values into my element variables?
